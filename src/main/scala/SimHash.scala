1ff66d000a952c996281cea8fa35eb8f9c569c84
Ludovic Guegan <ludo@titan.homeland>
2013-04-21 23:34:27 +0800

    add hash distance

--- src/main/scala/SimHash.scala
+++ src/main/scala/SimHash.scala
@@ -18,8 +18,17 @@ case class HashValue(val hash: Int) extends Ordered[HashValue] {
   def permutation(i: Int) = rotateLeft(i)
   def permutations = for (i <- 0 to 32) yield permutation(i)
   def compare(that: HashValue) = hash.compare(that.hash)
+  def distance(that: HashValue) = bitDiff(hash,that.hash)
   override def toString = "%32s".format(hash.toBinaryString).replace(' ', '0') 
   def toInt = hash
+  def bitDiff(a:Int, b: Int) = bitCount(~(a ^ b))
+  def bitCount(board:Int): Int = {
+    def bitIsSet(bit:Int) : Boolean = {
+      val mask = (1 << bit)
+      (board & mask) == mask
+    }
+    (0 until 32).filter(bitIsSet).length
+  }
 }
 
 object File {
@@ -34,17 +43,21 @@ case class File(val filename: String, val hash: HashValue) extends Ordered[File]
   def permutation(i: Int) = new File(filename, hash.permutation(i))
   def tokens: Seq[String] = File.tokens(filename)
   def compare(that: File) = hash.compare(that.hash)
+  def distance(that: File) = hash.distance(that.hash)
   override def toString = "%s (%d): %s, %s".format(hash, hash.toInt, filename, tokens.take(20).mkString(" "))
 }
 
 object HashSim {
 
   def main(args: Array[String]) = {
-
     val files: Seq[File] = fileList.map(file => File(file))
-    files.foreach(println)
     for (i <- 0 until 32) {
       val order = files.map(f => f.permutation(i)).sorted
+      order.foldLeft(new HashValue(0)){
+        (hash, file) => 
+          println("File " + file.filename + " distance " + file.hash.distance(hash).toString)
+          file.hash
+      }
     }
   }
 
